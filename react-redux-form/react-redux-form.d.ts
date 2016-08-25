e626e08963aa40a22e296a30ab6f77fad1199bd2
mdvorachek <mat.dvorachek@gisinc.com>
2016-08-25 18:29:38 -0400

    Minimal type definitions
    Initial commit of type definition file

---
+++ react-redux-form/react-redux-form.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for react-redux-form v0.14.4
+// Project https://github.com/davidkpiano/react-redux-form
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module 'react-redux-form' {
+    const ReactReduxForm: any;
+    export = ReactReduxForm;
+}
