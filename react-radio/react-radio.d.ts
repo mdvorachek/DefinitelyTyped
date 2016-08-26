a02e246f08342c950d7adcacd98907bca368bb93
mdvorachek <mat.dvorachek@gisinc.com>
2016-08-26 14:22:33 -0400

    Add minimal typing definitions

---
+++ react-radio/react-radio.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for react-radio v2.1.2
+// Project https://github.com/zippyui/react-radio
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module 'react-radio' {
+    const RadioGroup: any;
+    export = RadioGroup;
+}
