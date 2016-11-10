a9e04dc9e9eeb7028f58a5aa26e43b2613e02ac5
mdvorachek <mat.dvorachek@gisinc.com>
2016-11-10 18:18:47 -0500

    Add typings for redux-localstorage-simple

---
+++ redux-localstorage-simple/redux-localstorage-simple.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for redux-localstorage-simple v1.2.0
+// Project https://github.com/kilkelly/redux-localstorage-simple
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module 'redux-localstorage-simple' {
+    const save: any;
+    const load: any;
+}
