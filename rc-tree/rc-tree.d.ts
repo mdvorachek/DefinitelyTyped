cb2d45540d0b303d449a850973d2cd498ef166e1
mdvorachek <mat.dvorachek@gisinc.com>
2016-12-28 15:26:48 -0600

    Add typings for rc-tree

---
+++ rc-tree/rc-tree.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for rc-tree v1.4.1
+// Project https://github.com/react-component/tree
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module 'rc-tree' {
+    const Tree: any;
+    const TreeNode: any;
+    export default Tree;
+    export { TreeNode };
+}
