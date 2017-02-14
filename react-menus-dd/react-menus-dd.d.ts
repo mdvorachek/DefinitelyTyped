2f61bbf0fa7251e3936919ac9bab17d74570dcb1
mdvorachek <mat.dvorachek@gisinc.com>
2017-02-14 15:19:40 -0600

    Fix declaration name

--- react-menus-dd/react-menus-dd.d.ts
+++ react-menus-dd/react-menus-dd.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for react-menus-dd v2.1.0
+// Type definitions for react-menus-dd v1.0.1
 // Project https://github.com/dragfire/react-menus-dd
 // Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
-declare module 'react-contextmenu' {
+declare module 'react-menus-dd' {
     const Menu: any;
     const Menus: any;
 	const SubMenus: any;
