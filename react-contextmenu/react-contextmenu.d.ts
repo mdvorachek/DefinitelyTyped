93b6ab3fcf9f6103e1ae769ddd384626a353e8a8
mdvorachek <mat.dvorachek@gisinc.com>
2017-02-13 19:23:16 -0600

    Basic typings for react-contextmenu

---
+++ react-contextmenu/react-contextmenu.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for react-contextmenu v2.1.0
+// Project https://github.com/vkbansal/react-contextmenu
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module 'react-contextmenu' {
+    const ContextMenu: any;
+    const ContextMenuTrigger: any;
+	const MenuItem: any;
+	const SubMenu: any;
+    export { ContextMenu, ContextMenuTrigger, MenuItem, SubMenu };
+}
