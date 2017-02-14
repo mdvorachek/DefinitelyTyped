5e2a818864ec693be726048b28c2b0faaea730b1
mdvorachek <mat.dvorachek@gisinc.com>
2017-02-14 12:46:47 -0600

    Basic typings for react-menus-dd

---
+++ react-menus-dd/react-menus-dd.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for react-menus-dd v2.1.0
+// Project https://github.com/dragfire/react-menus-dd
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module 'react-contextmenu' {
+    const Menu: any;
+    const Menus: any;
+	const SubMenus: any;
+	
+    export { Menu, Menus, SubMenus };
+}
