7df950d6251f6377a158e0872cb3e0c922958ed3
mdvorachek <mat.dvorachek@gisinc.com>
2017-04-19 22:12:11 -0500

    Basic typings for ag-grid-react

---
+++ ag-grid-react/ag-grid-react.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for ag-grid-react v9.0.0
+// Project https://github.com/ceolter/ag-grid-react
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+export declare var AgGridReact: any;
