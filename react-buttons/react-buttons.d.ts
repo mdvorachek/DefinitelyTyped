94ba28a06672cec729be5571cb3e8cda04842683
mdvorachek <mat.dvorachek@gisinc.com>
2017-06-26 19:14:46 -0500

    Basic Typings for react-buttons

---
+++ react-buttons/react-buttons.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-buttons' {
+	const Button: any;
+	const IconButton: any;
+	const HamburgerButton: any;
+	const FloatingButton: any;
+	const FlatButton: any;
+	
+	export { Button, IconButton, HamburgerButton, FloatingButton, FlatButton };
+}
