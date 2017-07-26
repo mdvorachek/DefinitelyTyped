78aa2de8f275d527556e1e2899c322a3f238f1e2
mdvorachek <mat.dvorachek@gisinc.com>
2017-07-26 16:19:41 -0500

    Add comments

--- types/react-resizable/react-resizable.d.ts
+++ types/react-resizable/react-resizable.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for react-resizable v1.7.1
+// Project https://github.com/STRML/react-resizable
+// Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare module 'react-resizable' {
 	const Resizable: any;
 	const ResizableBox: any;
