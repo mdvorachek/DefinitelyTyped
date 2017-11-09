f5667feb619009575062b79cf05d6681fe41273d
mdvorachek <mat.dvorachek@gisinc.com>
2017-11-09 13:06:15 -0600

    Change typings to use react-radio-group
    Change typings to use react-radio-group

--- react-radio/react-radio-group.d.ts
+++ react-radio/react-radio-group.d.ts
@@ -1,10 +1,11 @@
-// Type definitions for react-radio v2.1.2
-// Project https://github.com/zippyui/react-radio
+// Type definitions for react-radio-group v3.0.2
+// Project https://github.com/chenglou/react-radio-group
 // Definitions by: Mat Dvorachek <https://github.com/mdvorachek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
-declare module 'react-radio' {
+declare module 'react-radio-group' {
     const RadioGroup: any;
+    const Radio: any;
     export default RadioGroup;
 }
